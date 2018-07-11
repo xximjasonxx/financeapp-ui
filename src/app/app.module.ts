88ffa6d8b68d7c23104925f9f1941d3391cd20d2
Jason Farrell <imjason@gmail.com>
2018-07-11 17:25:47 -0500

    added binding for sign up application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainLandingComponent } from './components/main-landing/main-landing.component';
@@ -29,7 +30,8 @@ const appRoutes: Routes =[
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true }
-    )
+    ),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,8 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { statesData } from './states';
+import { SignUpService } from '../../services/sign-up.service';
 
 class State {
   constructor(readonly abbr: string, readonly name: string) { }
+}
+
+class SignupApplication {
+  emailAddress: string;
+  password: string;
+  firstName: string;
+  address1: string;
+  address2: string;
+  city: string;
+  state: string;
+  zipcode: Number;
+  accountName: string;
+  accountType: string;
+  startingBalance: Number;
+
+  constructor() {
+    this.state = "";
+    this.accountType = "";
+  }
 }
 
 @Component({
@@ -12,10 +32,20 @@ class State {
 })
 export class SignUpComponent {
   states: Array<State>;
+  appData: SignupApplication;
+  isSubmitting: boolean;
 
-  constructor() {
+  constructor(private signupServie: SignUpService) {
     this.states = Object.keys(statesData).map((key) => {
       return new State(key, statesData[key]);
     });
+
+    this.appData = new SignupApplication();
+    this.isSubmitting = false;
+  }
+
+  submitApplication() {
+    debugger;
+    this.isSubmitting = true;
   }
 }
