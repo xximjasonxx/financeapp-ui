3c52d02495944e1083a9463213f1a1101ff4c7c1
Jason Farrell <imjason@gmail.com>
2018-09-29 22:19:42 -0500

    added total balance

--- src/app/components/accounts-list/accounts-list.component.ts
+++ src/app/components/accounts-list/accounts-list.component.ts
@@ -12,11 +12,15 @@ import { NewAccountDialogComponent } from './new-account-dialog/new-account-dial
 export class AccountsListComponent implements OnInit {
   isLoading: boolean;
   accountList: Array<AccountInfo>;
+  totalBalance: Number;
 
   constructor(
     private accountsService: AccountsService,
     private modalService: NgbModal
-  ) { }
+  ) {
+    this.accountList = [];
+    this.updateTotalBalance();
+  }
 
   ngOnInit() {
     this.loadAccountList();
@@ -27,12 +31,41 @@ export class AccountsListComponent implements OnInit {
     this.accountsService.getAccounts()
       .then((accounts) => {
         this.accountList = accounts;
+        this.updateTotalBalance();
         this.isLoading = false;
       })
       .catch((error) => {
         this.isLoading = false;
+        this.updateTotalBalance();
         alert(error.message);
       });
+
+      /*this.accountList = [
+        {
+            "accountId": "ad419bbb-eafa-4442-9964-25dede328d55",
+            "accountName": "Main Checking",
+            "currentBalance": 1000,
+            "statusCode": 1,
+            "status": "Open",
+            "accountType": "checking"
+        },
+        {
+            "accountId": "1ad0aee5-e639-49a8-8585-e9805bbb58d3",
+            "accountName": "My Retirement",
+            "currentBalance": 100000,
+            "statusCode": 1,
+            "status": "Open",
+            "accountType": "market"
+        },
+        {
+            "accountId": "02aef26b-4b38-4ecd-9e23-f8fa7167dee8",
+            "accountName": "My Savings",
+            "currentBalance": 40000,
+            "statusCode": 1,
+            "status": "Open",
+            "accountType": "savings"
+        }
+    ];*/
   }
 
   addAccount(): void {
@@ -48,5 +81,18 @@ export class AccountsListComponent implements OnInit {
   refreshAccountList(): void {
     this.accountList = [];
     this.loadAccountList();
+  }
+
+  updateTotalBalance() {
+    if (!this.accountList || this.accountList.length === 0) {
+      this.totalBalance = 0;
+    }
+    else {
+      this.totalBalance = this.accountList
+        .map((info) => info.currentBalance)
+        .reduce((total, value) => {
+          return Number(total) + Number(value);
+        });
+    }
   }
 }
